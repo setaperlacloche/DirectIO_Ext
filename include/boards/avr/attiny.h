1df626a21a48d91d0deebc46cbc8e55a9e0e4bfa
setaperlacloche <36166555+setaperlacloche@users.noreply.github.com>
2023-04-09 18:32:55 +0200

    Create attiny.h

---
+++ include/boards/avr/attiny.h
@@ -0,0 +1,7 @@
+// Support for attiny25 attiny45 attiny85
+_define_pin(0, PORT_B, 0);
+_define_pin(1, PORT_B, 1);
+_define_pin(2, PORT_B, 2);
+_define_pin(3, PORT_B, 3);
+_define_pin(4, PORT_B, 4);
+_define_pin(5, PORT_B, 5);
